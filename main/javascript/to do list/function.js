1bac7726824cda99c892d35f8140c2a9e19dc261
Morning <morning@admins-iMac.local>
2022-12-15 12:04:28 +0800

    1st

---
+++ main/javascript/to do list/function.js
@@ -0,0 +1,7 @@
+let input = document.getElementById("input");
+let button = document.getElementById("btn");
+let list = document.getElementById("list");
+let value;
+button.addEventListener("click", () => {
+  value = input.value;
+});
