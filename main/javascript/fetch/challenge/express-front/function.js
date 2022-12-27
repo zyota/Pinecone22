de53037c146ebacee0fdb31f4794556c95d3ab8b
Morning <morning@admins-iMac.local>
2022-12-27 12:08:16 +0800

    12/27

---
+++ main/javascript/fetch/challenge/express-front/function.js
@@ -0,0 +1,22 @@
+let btn = document.getElementById("btn");
+let root = document.getElementById("root");
+btn.addEventListener("click", () => {
+  fetch("http://localhost:3333/api/cars", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((car) => {
+        let newCard = document.createElement("div");
+        newCard.innerHTML = `<div class="card" style="width: 18rem;">
+        <img src="${car.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${car.model}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`;
+        root.appendChild(newCard);
+      });
+    });
+});
